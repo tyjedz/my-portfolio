73360f62e9e25df31486526fdafab9bb83991532
Tyler Jedziniak <tyjedz@gmail.com>
2021-06-16 22:35:05 +0000

    Create script which shows random message using JSON

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,18 +1,29 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-
+  private ArrayList<String> facts = new ArrayList<String>();
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Tyler Jedziniak</h1>");
+    if (facts.size() < 3) {
+      facts.add("My name is Tyler.");
+      facts.add("I play guitar.");
+      facts.add("I go to Georgia Tech.");
+    }
+    Gson gson = new Gson();
+    String json = gson.toJson(facts);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
