8af11d11263adf930b577433c3afc6835c95cba3
Tyler Jedziniak <tyjedz@gmail.com>
2021-06-22 03:31:16 +0000

    Refactor list element in HelloWorldServlet.java

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -16,7 +16,7 @@ public class FormHandlerServlet extends HttpServlet {
     String textValue = request.getParameter("text-input");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("User submitted submitted: " + textValue);
+    System.out.println("User submitted: " + textValue);
 
     // Write the value to the response so the user can see it.
     response.getWriter().println("You submitted: " + textValue);
--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -2,6 +2,8 @@ package com.google.sps.servlets;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,14 +15,12 @@ import com.google.gson.Gson;
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-  private ArrayList<String> facts = new ArrayList<String>();
+  
+  List<String> facts = Arrays.asList("My name is Tyler.", "I play guitar.", "I go to Georgia Tech.");
+  
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    if (facts.size() < 3) {
-      facts.add("My name is Tyler.");
-      facts.add("I play guitar.");
-      facts.add("I go to Georgia Tech.");
-    }
+    
     Gson gson = new Gson();
     String json = gson.toJson(facts);
     response.setContentType("application/json;");
